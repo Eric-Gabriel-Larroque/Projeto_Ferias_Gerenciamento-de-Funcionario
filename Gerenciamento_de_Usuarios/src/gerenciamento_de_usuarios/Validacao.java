6acf14415ab889fa048a407244240141fde3ea84
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2021-12-28 22:44:04 -0300

    criando novas funções de validação para melhor manipulação das exceções

--- Gerenciamento_de_Usuarios/src/gerenciamento_de_usuarios/Validacao.java
+++ Gerenciamento_de_Usuarios/src/gerenciamento_de_usuarios/Validacao.java
@@ -11,26 +11,30 @@ public class Validacao {
 
         while(!RG.matches(RG_TEMPLATE)) {
                 RG = entradaVazia(JOptionPane.showInputDialog(null,"O registro geral contém " +
-                        "10 números, serve para distinguir um funcionário do outro. Insira novamente: "));
+                        "10 números, serve para distinguir um funcionário do outro. Insira novamente: "),
+                        "Digite novamente o RG:");
         }
         return Long.parseLong(RG);
     }
 
-    public int somenteNumeros(String entrada) {
+    public int somenteNumeros(String entrada, String errorLabel) {
         String SOMENTE_NUMEROS_TEMPLATE = "[0-9]+";
         while(!entrada.matches(SOMENTE_NUMEROS_TEMPLATE)){
             entrada = entradaVazia(JOptionPane.showInputDialog(null,
-                    "Somente números são aceitos. Insira novamente: "));
+                    "Somente números são permitidos. "+errorLabel),
+                    errorLabel);
+            entrada = entrada.length()>10?entrada.substring(0,6):entrada;
         }
         return Integer.parseInt(entrada);
     }
 
-    public String entradaVazia(String entrada) {
-        entrada = String.valueOf(entrada);
-        while(entrada.isEmpty()||entrada.isBlank()){
-            entrada = JOptionPane.showInputDialog(null,"Entrada de dados vazia. Digite novamente: ");
+        public String entradaVazia(String entrada, String errorLabel) {
             entrada = String.valueOf(entrada);
+            while (entrada.isEmpty() || entrada.isBlank()) {
+                entrada = JOptionPane.showInputDialog(null,
+                        "Entrada vazia. "+errorLabel);
+                entrada = String.valueOf(entrada);
+            }
+            return entrada;
         }
-        return entrada;
-    }
 }
